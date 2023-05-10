380139eeeb0353877bdcc7b54ef181a74a6f55cc
Andrey Safonov <a.so777@live.ca>
2023-05-10 13:51:18 -0700

    Added demos and trial keys

--- components/WebViewer.vue
+++ components/WebViewer.vue
@@ -16,6 +16,7 @@ export default {
         WebViewer({
             path: '../webviewer',
             initialDoc: this.url, // replace with your own PDF file
+            licenseKey: 'your_license_key'  // sign up to get a free trial key at https://dev.apryse.com
           }, this.$refs.viewer).then((instance) => {
             // call apis here
           });
